45eb4525f2f75bd909eb24924dbbedb40eaa4af2
BHAVISHAYA <bhavishayakhandelwal@gmail.com>
2024-12-28 08:50:26 +0530

    Introduction To GIT

--- 1_introToGitAndHitHub.js
+++ 1_introToGitAndHitHub.js
@@ -1,1 +1,8 @@
-//* Introduction to git hub 
+//? What is Git ?
+//* Git is a distributed version control system (DVCS) that helps developers track changes in their source code during software development. 
+
+//* Git is specifically classified as a DVCS. In a DVCS, each developer has a complete copy of the entire project, including its full history. This is in contrast to Centralized Version Control Systems(CVCS), where there is a single central repository. 
+
+
+//? What is Version Control System ? 
+//* A VCS is a tool or system that helps developers manage changes to their source code over time. It allows developers to track modifications, revert to previous stages, and work collaboratively with others on the same codebase. 
